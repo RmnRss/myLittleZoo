19a78c048b7405abb8bb80bd27661085975378ff
Rousseau Romain <romain.rousseau5@gmail.com>
2018-10-22 18:11:35 +0200

    fin 22/10

--- src/main/java/Zoo/Zoo.java
+++ src/main/java/Zoo/Zoo.java
@@ -3,12 +3,17 @@ package Zoo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import Animaux.Animal;
 import Exception.AnimalDansMauvaisSecteurException;
 import Exception.LimiteVisiteursException;
 
 public class Zoo 
 {
+    private static final Logger LOGGER = LogManager.getLogger(Zoo.class.getName());
+
 	private int visiteurs;
 	private List<Secteur> secteursAnimaux = new ArrayList<Secteur>();
 	
@@ -22,9 +27,10 @@ public class Zoo
 	public void nouveauVisiteur() throws LimiteVisiteursException
 	{
 		int maxVisiteurs = secteursAnimaux.size()*15;
-		
+
 		if (visiteurs >= maxVisiteurs) 
 		{
+			LOGGER.error("Plus de place dans le Zoo Frero !");
 			throw new LimiteVisiteursException("Plus de place dans le Zoo !");
 		} 
 		else 
@@ -45,6 +51,7 @@ public class Zoo
 	
 	public void nouvelAnimal(String nom, String type) throws AnimalDansMauvaisSecteurException
 	{
+		LOGGER.info("ajout d'un nouvel animal");
 		this.secteursAnimaux.get(0).ajouterAnimal(new Animal(nom, type));
 	}
 	
@@ -57,6 +64,7 @@ public class Zoo
 			nbr_animaux += secteursAnimaux.get(i).getNombreAnimaux();
 		}
 		
+		LOGGER.trace("nbr animaux " + nbr_animaux);
 		return nbr_animaux;
 	}
 }
